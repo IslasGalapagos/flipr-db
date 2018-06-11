1b15394fe730c00dcb899f2db244fc6e6d2407eb
Evgeny Sysoletin <evgeny.sysoletin@gmail.com>
2018-06-11 22:23:25 +0300

    update insert_user to use pgcrypto

--- revert/insert_user.sql
+++ revert/insert_user.sql
@@ -1,7 +1,14 @@
--- Revert flipr:insert_user from pg
+-- Deploy flipr:insert_user to pg
+-- requires: appschema
+-- requires: users
 
 BEGIN;
 
-DROP FUNCTION flipr.insert_user(TEXT, TEXT);
+CREATE OR REPLACE FUNCTION flipr.insert_user(
+  nickname TEXT,
+  password TEXT
+) RETURNS VOID LANGUAGE SQL SECURITY DEFINER AS $$
+  INSERT INTO flipr.users VALUES ($1, md5($2));
+$$;
 
 COMMIT;
--- test/insert_user.sql
+++ test/insert_user.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS pgtap;
 RESET client_min_messages;
 
 BEGIN;
-SELECT plan(11);
+SELECT plan(12);
 SET search_path TO flipr,public;
 
 SELECT has_function('insert_user');
@@ -12,19 +12,23 @@ SELECT function_lang_is('insert_user', ARRAY['text', 'text'], 'sql');
 SELECT function_returns('insert_user', ARRAY['text', 'text'], 'void');
 SELECT volatility_is('insert_user', ARRAY['text', 'text'], 'volatile');
 SELECT lives_ok($$ SELECT insert_user('evgenys', 'foo') $$, 'Insert user');
-SELECT row_eq(
-  'SELECT * FROM users', 
-  ROW('evgenys', md5('foo'), NOW())::users,
+SELECT ok(
+  EXISTS(
+    SELECT 1 FROM flipr.users
+      WHERE nickname = 'evgenys'
+      AND password = crypt('foo', password)
+  ),
   'The user should have been inserted'
 );
 SELECT lives_ok($$ SELECT insert_user('islas', 'bar') $$, 'Insert user');
-SELECT bag_eq(
-  'SELECT * FROM users',
-  $$ VALUES
-    ('evgenys', md5('foo'), NOW()),
-    ('islas', md5('bar'), NOW())
-  $$,
-  'Both users should be present'
+SELECT is(count(*)::INT, 2, 'There should be two users') FROM flipr.users;
+SELECT ok(
+  EXISTS(
+    SELECT 1 FROM flipr.users
+      WHERE nickname = 'islas'
+      AND password = crypt('bar', password)
+  ),
+  'The user should have been inserted'
 );
 SELECT throws_ok(
   $$ SELECT insert_user('evgenys', 'fooo') $$,
@@ -32,14 +36,7 @@ SELECT throws_ok(
   NULL,
   'Should get an error for dublicate nickname'
 );
-SELECT bag_eq(
-  'SELECT * FROM users',
-  $$ VALUES
-    ('evgenys', md5('foo'), NOW()),
-    ('islas', md5('bar'), NOW())
-  $$,
-  'Should still have just the two users'
-);
+SELECT is(count(*)::INT, 2, 'There should be two users') FROM flipr.users;
 
 SELECT finish();
 ROLLBACK;
